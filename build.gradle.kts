f177407885f17c5775574a3e17d65daa3d6d4e90
Flyfish233 <37693645+Flyfish233@users.noreply.github.com>
2024-03-16 20:53:54 +0800

    Add dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    alias(libs.plugins.dokka)
     id("root.publication")
 }

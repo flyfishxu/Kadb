9b66db53079b032566262e48ca7a3a5f892e8ed9
Flyfish233 <37693645+Flyfish233@users.noreply.github.com>
2024-03-17 23:43:39 +0800

    TODO For better conscrypt

--- kadb/src/androidMain/kotlin/pair/PairingConnectionCtx.android.kt
+++ kadb/src/androidMain/kotlin/pair/PairingConnectionCtx.android.kt
@@ -3,7 +3,7 @@ package com.flyfishxu.kadb.pair
 import android.annotation.SuppressLint
 import android.os.Build
 import com.flyfishxu.kadb.SslUtils
-import javax.net.ssl.SSLException
+import java.security.NoSuchAlgorithmException
 
 @SuppressLint("PrivateApi")
 actual fun PairingConnectionCtx.getConscryptClass(): Class<*> {
@@ -12,7 +12,7 @@ actual fun PairingConnectionCtx.getConscryptClass(): Class<*> {
     } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
         // Although support for conscrypt has been added in Android 5.0 (Lollipop),
         // TLS1.3 isn't supported until Android 9 (Pie).
-        throw SSLException("TLSv1.3 isn't supported on your platform. Use custom Conscrypt library instead.")
+        throw NoSuchAlgorithmException("TLSv1.3 isn't supported on your platform. Use custom Conscrypt library instead.")
     } else {
         Class.forName("com.android.org.conscrypt.Conscrypt")
     }

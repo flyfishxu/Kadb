c5c336118fb956fa000e8aa442701184a04c14b3
Flyfish233 <37693645+Flyfish233@users.noreply.github.com>
2024-03-17 23:32:35 +0800

    JVM File Op fix

--- kadb/src/jvmMain/kotlin/Kadb.jvm.kt
+++ kadb/src/jvmMain/kotlin/Kadb.jvm.kt
@@ -1,17 +1,10 @@
 package com.flyfishxu.kadb
 
 import java.io.File
+import java.nio.file.Files
 
 actual fun Kadb.readMode(file: File): Int {
-    var mode = 0
-    if (file.canRead()) {
-        mode = mode or 400
-    }
-    if (file.canWrite()) {
-        mode = mode or 200
-    }
-    if (file.canExecute()) {
-        mode = mode or 100
-    }
-    return mode
+    return Files.getAttribute(
+        file.toPath(), "unix:mode"
+    ) as? Int ?: throw RuntimeException("Unable to read file mode")
 }

c163165df02b36506b13c749433b3e5725db3e11
Flyfish Xu <41728565+flyfishxu@users.noreply.github.com>
2024-09-25 16:02:15 +0800

    Configured Maven

--- convention-plugins/src/main/kotlin/module.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/module.publication.gradle.kts
@@ -1,3 +1,5 @@
+apply(from = "../../../../publish.gradle.kts")
+
 plugins {
     `maven-publish`
     signing
@@ -12,30 +14,49 @@ publishing {
             archiveAppendix.set(this@withType.name)
         })
 
+        artifact(tasks.register("${name}SourceJar", Jar::class) {
+            archiveClassifier.set("sources")
+            archiveAppendix.set(this@withType.name)
+            //from(android.sourceSets["main"].java.srcDirs)
+        })
+
         // Provide artifacts information required by Maven Central
         pom {
-            name.set("Kotlin Multiplatform library template")
-            description.set("Dummy library to test deployment to Maven Central")
-            url.set("https://github.com/Kotlin/multiplatform-library-template")
+            name.set("Kadb")
+            description.set("A modern and versatile Kotlin ADB client library that simplifies the interaction with Android devices.")
+            url.set("https://github.com/flyfishxu/Kadb.git")
 
             licenses {
                 license {
-                    name.set("MIT")
-                    url.set("https://opensource.org/licenses/MIT")
+                    name.set("Apache License 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
                 }
             }
             developers {
                 developer {
-                    id.set("JetBrains")
-                    name.set("JetBrains Team")
-                    organization.set("JetBrains")
-                    organizationUrl.set("https://www.jetbrains.com")
+                    id.set("flyfishxu")
+                    name.set("Flyfish Xu")
+                    organization.set("Flyfish Studio")
+                    email.set( "flyfishxu@outlook.com")
                 }
             }
             scm {
-                url.set("https://github.com/Kotlin/multiplatform-library-template")
+                connection.set("scm:git:git://github.com/flyfishxu/Kadb.git")
+                developerConnection.set("scm:git:ssh://github.com/flyfishxu/Kadb.git")
+                url.set("https://github.com/flyfishxu/Kadb.git")
             }
         }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                username = extra["sonatypeUsername"] as String
+                password = extra["sonatypePassword"] as String
+            }
+        }
+        mavenLocal()
     }
 }
 

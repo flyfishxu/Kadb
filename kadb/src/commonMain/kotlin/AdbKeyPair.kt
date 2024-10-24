18d1c2d864d4bb8115335b5a93bee8b18d165e1c
Flyfish Xu <41728565+flyfishxu@users.noreply.github.com>
2024-10-25 01:10:51 +0800

    Sync

--- kadb/src/commonMain/kotlin/AdbKeyPair.kt
+++ kadb/src/commonMain/kotlin/AdbKeyPair.kt
@@ -30,9 +30,12 @@ import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder
 import java.io.File
 import java.io.FileInputStream
 import java.math.BigInteger
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import java.security.*
 import java.security.cert.Certificate
 import java.security.cert.CertificateFactory
+import java.security.interfaces.RSAPublicKey
 import java.util.*
 import javax.crypto.Cipher
 import kotlin.io.encoding.Base64
@@ -46,7 +49,7 @@ private const val CERT_END = "-----END CERTIFICATE-----"
 
 
 class AdbKeyPair(
-    val privateKey: PrivateKey, val publicKey: PublicKey, val certificate: Certificate
+    val privateKey: PrivateKey, val certificate: Certificate
 ) {
     internal fun signPayload(message: AdbMessage): ByteArray {
         val cipher = Cipher.getInstance("RSA/ECB/NoPadding")
@@ -55,7 +58,70 @@ class AdbKeyPair(
         return cipher.doFinal(message.payload, 0, message.payloadLength)
     }
 
+    fun
+            getPublicKeyBytes(): ByteArray {
+        val adbFormatBytes = convertRsaPublicKeyToAdbFormat(certificate.publicKey as RSAPublicKey)
+        val publicKeyBytes = adbFormatBytes.copyOf(adbFormatBytes.size + 1)
+        publicKeyBytes[adbFormatBytes.size] = 0
+        return adbFormatBytes
+    }
+
     companion object {
+        private const val KEY_LENGTH_BITS = 2048
+        private const val KEY_LENGTH_BYTES = KEY_LENGTH_BITS / 8
+        private const val KEY_LENGTH_WORDS = KEY_LENGTH_BYTES / 4
+
+        // https://github.com/cgutman/AdbLib/blob/d6937951eb98557c76ee2081e383d50886ce109a/src/com/cgutman/adblib/AdbCrypto.java#L83-L137
+        @Suppress("JoinDeclarationAndAssignment")
+        private fun convertRsaPublicKeyToAdbFormat(pubKey: RSAPublicKey): ByteArray {
+            /*
+             * ADB literally just saves the RSAPublicKey struct to a file.
+             *
+             * typedef struct RSAPublicKey {
+             * int len; // Length of n[] in number of uint32_t
+             * uint32_t n0inv;  // -1 / n[0] mod 2^32
+             * uint32_t n[RSANUMWORDS]; // modulus as little endian array
+             * uint32_t rr[RSANUMWORDS]; // R^2 as little endian array
+             * int exponent; // 3 or 65537
+             * } RSAPublicKey;
+             */
+
+            /* ------ This part is a Java-ified version of RSA_to_RSAPublicKey from adb_host_auth.c ------ */
+            val r32: BigInteger
+            val r: BigInteger
+            var rr: BigInteger
+            var rem: BigInteger
+            var n: BigInteger
+            val n0inv: BigInteger
+            r32 = BigInteger.ZERO.setBit(32)
+            n = pubKey.modulus
+            r = BigInteger.ZERO.setBit(KEY_LENGTH_WORDS * 32)
+            rr = r.modPow(BigInteger.valueOf(2), n)
+            rem = n.remainder(r32)
+            n0inv = rem.modInverse(r32)
+            val myN = IntArray(KEY_LENGTH_WORDS)
+            val myRr = IntArray(KEY_LENGTH_WORDS)
+            var res: Array<BigInteger>
+            for (i in 0 until KEY_LENGTH_WORDS) {
+                res = rr.divideAndRemainder(r32)
+                rr = res[0]
+                rem = res[1]
+                myRr[i] = rem.toInt()
+                res = n.divideAndRemainder(r32)
+                n = res[0]
+                rem = res[1]
+                myN[i] = rem.toInt()
+            }
+
+            /* ------------------------------------------------------------------------------------------- */
+            val bbuf: ByteBuffer = ByteBuffer.allocate(524).order(ByteOrder.LITTLE_ENDIAN)
+            bbuf.putInt(KEY_LENGTH_WORDS)
+            bbuf.putInt(n0inv.negate().toInt())
+            for (i in myN) bbuf.putInt(i)
+            for (i in myRr) bbuf.putInt(i)
+            bbuf.putInt(pubKey.publicExponent.toInt())
+            return bbuf.array()
+        }
 
         private fun readCertificateFromFile(): Certificate? {
             // TODO: DO NOT HARD CODE CERT FILE
@@ -76,7 +142,7 @@ class AdbKeyPair(
             val privateKey = readPrivateKeyFromFile()
             val certificate = readCertificateFromFile()
             return if (privateKey == null || certificate == null) generate()
-            else AdbKeyPair(privateKey, certificate.publicKey, certificate)
+            else AdbKeyPair(privateKey, certificate)
         }
     }
 }
@@ -145,7 +211,7 @@ fun AdbKeyPair.Companion.generate(
     writePrivateKeyToFile(privateKey)
     writeCertificateToFile(certificate)
 
-    return AdbKeyPair(privateKey, publicKey, certificate)
+    return AdbKeyPair(privateKey, certificate)
 }
 
 

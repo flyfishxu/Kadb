bd0ba4fad74c918facd298419ff801bb13af6e65
flyfishxu <flyfishxu@outlook.com>
2026-03-27 16:42:16 +0800

    build: migrate to Android KMP library plugin and update toolchain/dependencies
    Switch to com.android.kotlin.multiplatform.library plugin, update Android target DSL, add daemon JDK toolchain config, enable GPG signing, and bump Kotlin/Okio/maven-publish versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.androidLibrary).apply(false)
+    alias(libs.plugins.androidMultiplatformLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.dokka)
-    id("com.vanniktech.maven.publish") version "0.34.0" apply false
+    id("com.vanniktech.maven.publish") version "0.36.0" apply false
 }
